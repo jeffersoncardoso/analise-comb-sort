6aed071399355d3ded66872a4272f5ee200c5a14
Jefferson Cardoso <jefferson.mcardoso@yahoo.com.br>
2017-06-15 13:38:55 -0300

    Ajustando identação do código

--- comb_sort.c
+++ comb_sort.c
@@ -6,11 +6,11 @@
 
 void mostrar(int * vetor){
 	int i = 0;
-	
+
 	for(i = 0; i < TAM; i++){
 		printf("%d ", vetor[i]);
 	}
-	
+
 	printf("%\n");
 }
 
@@ -18,9 +18,9 @@ int * gerarVetor(int vetor[]){
 	int i;
 
 	srand((unsigned)time(0));
-    int maior = 10000;
-    int menor = 1;
-    int aleatorio = 0;
+	int maior = 10000;
+	int menor = 1;
+	int aleatorio = 0;
 
 	for(i = 0; i < TAM; i++){
 		vetor[i] = rand()%(maior-menor+1) + menor;
@@ -32,14 +32,14 @@ int * gerarVetor(int vetor[]){
 
 int calcularGap(int gap){
 	gap = gap / 1.3; //Fator de encolhimento
-	if(gap < 1) return 1; //Caso a divis„o do gap pelo fator de encolhimento seja menor que um, ent„o o gap ser· 1(distancia de um elemento)
+	if(gap < 1) return 1; //Caso a divis√£o do gap pelo fator de encolhimento seja menor que um, ent√£o o gap ser√° 1(distancia de um elemento)
 	
 	return gap;
 }
 
 void trocarLugares(int * primeiro, int * segundo){ //Utilizo o conceito de ponteiro para realizar a troca dos valores
 	int auxiliar = 0;
-	auxiliar = *primeiro; //Coloco o valor do primeiro na vari·vel auxiliar
+	auxiliar = *primeiro; //Coloco o valor do primeiro na vari√°vel auxiliar
 	*primeiro = *segundo; //atribuio o valor do segundo no primeiro
 	*segundo = auxiliar; //atribuio o valor do auxiliar ao segundo
 }
@@ -47,29 +47,30 @@ void trocarLugares(int * primeiro, int * segundo){ //Utilizo o conceito de ponte
 void combSort(int * vetor, int tamanho){
 	int i = 0;
 	int gap = tamanho; //Inicializo o gap com o tamanho do vetor;
-	//O gap È o espaÁo entre o elemento de referencia e o elemento que irei compara-lo
-	//em algoritmos como o bubble sort esse gap È igual a 1(vizinho com vizinho).
-	//No comb sort esse espaÁo È calculado baseado em um fator de encolhimento que por convenÁ„o e simulaÁıes È igual a 1,3
+	//O gap √© o espa√ßo entre o elemento de referencia e o elemento que irei compara-lo
+	//em algoritmos como o bubble sort esse gap √© igual a 1(vizinho com vizinho).
+	//No comb sort esse espa√ßo √© calculado baseado em um fator de encolhimento que por conven√ß√£o e simula√ß√µes √© igual a 1,3
 	
-	int houveTrocaDeLugares; //vari·vel flag, que ir· monitorar se houve ou n„o troca de lugares entre os elementos
+	int houveTrocaDeLugares; //vari√°vel flag, que ir√° monitorar se houve ou n√£o troca de lugares entre os elementos
 	
 	do{
-		gap = calcularGap(gap); //Calculo o valor do gap a cada iteraÁ„o
-		houveTrocaDeLugares = false; //Defino o valor da flag como falso, se houverem trocas se tornar· verdadeiro
+		gap = calcularGap(gap); //Calculo o valor do gap a cada itera√ß√£o
+		houveTrocaDeLugares = false; //Defino o valor da flag como falso, se houverem trocas se tornar√° verdadeiro
 		
 		for(i = 0; i < tamanho - gap; i++){ //Percorro o vetor utilizando o gap como referencia
 			
-			if ( vetor[i] > vetor[i + gap] ){ //Comparo cada elemento com o elemento com 'gap' posiÁıes ao lado
-				trocarLugares(&vetor[i], &vetor[i + gap]); //Caso seja maior, faÁo a troca dos elementos
-                houveTrocaDeLugares = true; //e a flag vir· true
-            }
+			if ( vetor[i] > vetor[i + gap] ){ //Comparo cada elemento com o elemento com 'gap' posi√ß√µes ao lado
+				trocarLugares(&vetor[i], &vetor[i + gap]); //Caso seja maior, fa√ßo a troca dos elementos
+				houveTrocaDeLugares = true; //e a flag vir√° true
+			}
             
 		}
+		
 	}while(gap != 1 || houveTrocaDeLugares == true); 
-	//Tudo isso ser· executado enquanto o gap (distancia) for diferente de 1 e n„o houver tido troca de lugares na iteraÁ„o
-	//Quanto mais iteraÁıes forem executadas, menor ser· o gap, atÈ o ponto que ele ser· igual a 1
-	//nesse momento ainda podem haver elementos desordenados, por isso o vetor sÛ estar· completamente
-	//ordenado quando alÈm de o gap ser igual a 1, n„o houver tido nenhuma troca de lugar na iteraÁ„o.
+	//Tudo isso ser√° executado enquanto o gap (distancia) for diferente de 1 e n√£o houver tido troca de lugares na itera√ß√£o
+	//Quanto mais itera√ß√µes forem executadas, menor ser√° o gap, at√© o ponto que ele ser√° igual a 1
+	//nesse momento ainda podem haver elementos desordenados, por isso o vetor s√≥ estar√° completamente
+	//ordenado quando al√©m de o gap ser igual a 1, n√£o houver tido nenhuma troca de lugar na itera√ß√£o.
 }
 
 
